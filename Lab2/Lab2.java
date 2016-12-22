0736bebaa84783e2c5f7eee50690fc5323d449ae
WenjingWangCarrie <wenjing.liu.wang@gmail.com>
2016-12-21 22:07:28 -0500

    Add files via upload

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Lab2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int number = 0;
+		
+		System.out.println("Enter a number");
+		number = sc.nextInt();
+		
+		System.out.println("The first three multiple of " + number + " are: " + number);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+	}
+
+}

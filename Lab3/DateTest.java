6e1ba042fad36087c5b16639f8a7933a9e7847f5
WenjingWangCarrie <wenjing.liu.wang@gmail.com>
2016-12-21 22:08:10 -0500

    Add files via upload

---
+++ Lab3/DateTest.java
@@ -0,0 +1,15 @@
+
+public class DateTest {
+
+	public static void main(String[] args) {
+
+		OurDate date = new OurDate();
+
+		System.out.println("Initial date is " + date.toString());
+		date.display();
+		System.out.println("Date is " + date.toString());
+		
+
+	}
+
+}
--- Lab3/OurDate.java
+++ Lab3/OurDate.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class OurDate {
+
+	int month;
+	int day;
+	int year;
+	
+	public OurDate() {
+		month = 1;
+		day = 1;
+		year = 1901;
+	}
+	
+	public OurDate(int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+	}
+	
+	public void display() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a month: ");
+		month = sc.nextInt();
+		System.out.println("Enter a day: ");
+		day = sc.nextInt();
+		System.out.println("Enter a year: ");
+		year = sc.nextInt();
+	}
+
+	public String toString() {
+		return this.month + "/" + this.day + "/" + this.year; 
+	}
+}

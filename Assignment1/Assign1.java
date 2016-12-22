d542ad563cdbfbebf333578399a34eff79273d7d
WenjingWangCarrie <wenjing.liu.wang@gmail.com>
2016-12-21 23:03:17 -0500

    Add files via upload

--- Assignment1/Assign1.java
+++ Assignment1/Assign1.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Assign1 {
+
+	public static void main(String[] args) {
+
+		double hybrid, lab, assign, quiz, test, finalExam;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Welcome to the CST8110 Final Mark Calculation");
+		System.out.println("Enter your hybrid activity mark out of 5:");
+		hybrid = sc.nextDouble();
+
+		System.out.println("Enter your lab mark out of 10:");
+		lab = sc.nextDouble();
+
+		System.out.println("Enter your assignments mark out of 100:");
+		assign = sc.nextDouble();
+
+		System.out.println("Enter your quiz mark out of 25:");
+		quiz = sc.nextDouble();
+
+		System.out.println("Enter your lab test mark out of 5:");
+		test = sc.nextDouble();
+
+		System.out.println("Enter your final exam mark out of 30:");
+		finalExam = sc.nextDouble();
+
+		double calculate = hybrid + lab + assign * 0.2 + quiz + test * 2 + finalExam;
+		System.out.println("Calculated final mark: " + calculate);
+	}
+
+}

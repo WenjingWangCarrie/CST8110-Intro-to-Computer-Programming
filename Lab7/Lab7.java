32d5a457034ba181d000f71d651c6938b17f06b6
WenjingWangCarrie <wenjing.liu.wang@gmail.com>
2017-01-03 23:02:36 -0500

    Add files via upload

--- Lab7/Lab7.java
+++ Lab7/Lab7.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class Lab7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String array;
+		int upper = 0;
+		int lower = 0;
+		int others = 0;
+
+		System.out.println("Enter the phrase: ");
+		array = sc.nextLine();
+		System.out.println("The total number of letters is " + array.length());
+
+		for (int i = 0; i < array.length(); i++) {
+			if (Character.isUpperCase(array.charAt(i))) {
+				upper++;
+			} else if (Character.isLowerCase(array.charAt(i))) {
+				lower++;
+			} else {
+                others++;
+			}
+
+		}
+		System.out.println("The number of upper case letters is " + upper);
+		System.out.println("The number of lower case letters is " + lower);
+		System.out.println("The number of other letters is " + others);
+	}
+
+}
